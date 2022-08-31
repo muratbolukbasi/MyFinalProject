4e37d6948dc6fec8e89dbdf83786c0f4647a0f9b
Murat Bölükbaşı <muratbolukbasi74@outlook.com>
2022-08-31 15:48:06 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind,Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }     //Db deki Product kolonu Products a
+        public DbSet<Category> Categories { get; set; } //Db deki Category kolunu Categories e bağlanıyor
+        public DbSet<Customer> Customers { get; set; } //Db deki Customer kolonu Customers e 
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    //ÇIPLAK CLASS KALMASIN
+    public class Category: IEntity
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; } 
+    }
+}
